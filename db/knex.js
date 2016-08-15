9d5a17a3166565bf210e49fc2af1b06e3066aa07
kiran koirala <kinbech@gmail.com>
2016-08-15 05:26:59 -0400

    knex for dev

--- db/knex.js
+++ db/knex.js
@@ -1,4 +1,4 @@
-var environment =  process.env.NODE_ENV || 'local';'//development';
+var environment =  'development';//process.env.NODE_ENV || 'local';//
 var config = require('../knexfile.js')[environment];
 
 module.exports = require('knex')(config);
